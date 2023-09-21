b3aa11cdd6ba2e17e0bd32b7a2ce7d1675c19880
torbenconto <torben@2x1.dev>
2023-09-20 23:30:27 -0400

    add todo

--- ls.go
+++ ls.go
@@ -30,7 +30,9 @@ func Command_ls() Command {
 			dir := filepath.Join(env.Pwd, arg.value)
 
 			if fs.DirExists(dir) {
+				// TODO(torbenconto): add support for -la and other flags, use seperate ls functions to list different types of files
 				fs.ListDir(dir)
+				return env, nil
 
 			} else {
 				return nil, errors.New("directory does not exist")
