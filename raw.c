066c9e30002350e28cfb72b748aec924dd89dec6
torbenconto <torben@2x1.dev>
2023-10-03 13:08:49 -0400

    disable sofware control flow inputs

--- raw.c
+++ raw.c
@@ -13,6 +13,12 @@ void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios rawmode = orig_termios;
+
+  // Disable software control flow inputs
+  rawmode.c_iflag &= ~(IXON);
+
+  // Disable echo, canonical mode, and SIG keys like CTRL + C
   rawmode.c_lflag &= ~(ECHO | ICANON | ISIG);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawmode);
 }
