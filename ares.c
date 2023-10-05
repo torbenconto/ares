b7745a161e5e3b64fb0d44aca027daa7ed9e7a98
torbenconto <torben@2x1.dev>
2023-10-05 03:49:31 -0400

    highlight strings, start comment highlighting

--- ares.c
+++ ares.c
@@ -29,7 +29,9 @@ enum Key {
 enum Highlight {
   HL_NORMAL = 0,
   HL_NUMBER,
-  HL_MATCH
+  HL_MATCH,
+  HL_STRING,
+  HL_COMMENT
 };
 
 struct Syntax {
@@ -71,7 +73,7 @@ struct Syntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
-    HL_HIGHLIGHT_NUMBERS
+    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
 
@@ -192,18 +194,36 @@ int is_separator(int c) {
 }
 
 void updateSyntax(erow *row) {
-  row->hl = realloc(row->hl, row->rsize);
+    row->hl = realloc(row->hl, row->rsize);
   memset(row->hl, HL_NORMAL, row->rsize);
-
   if (S.syntax == NULL) return;
-
   int prev_sep = 1;
-
+  int in_string = 0;
   int i = 0;
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-
+    if (S.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+      if (in_string) {
+        row->hl[i] = HL_STRING;
+        if (c == '\\' && i + 1 < row->rsize) {
+          row->hl[i + 1] = HL_STRING;
+          i += 2;
+          continue;
+        }
+        if (c == in_string) in_string = 0;
+        i++;
+        prev_sep = 1;
+        continue;
+      } else {
+        if (c == '"' || c == '\'') {
+          in_string = c;
+          row->hl[i] = HL_STRING;
+          i++;
+          continue;
+        }
+      }
+    }
     if (S.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
       if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
           (c == '.' && prev_hl == HL_NUMBER)) {
@@ -213,7 +233,6 @@ void updateSyntax(erow *row) {
         continue;
       }
     }
-
     prev_sep = is_separator(c);
     i++;
   }
@@ -221,7 +240,9 @@ void updateSyntax(erow *row) {
 
 int syntaxToColor(int hl) {
   switch (hl) {
+    case HL_STRING: return STRING_HIGHLIGHT_COLOR;
     case HL_NUMBER: return NUMBER_HIGHLIGHT_COLOR;
+    case HL_COMMENT: return COMMENT_HIGHLIGHT_COLOR;
     case HL_MATCH: return SEARCH_MATCH_HIGHLIGHT_COLOR;
     default: return DEFAULT_HIGHLIGHT_COLOR;
   }
--- ares.h
+++ ares.h
@@ -4,8 +4,10 @@
 #define ARES_VERSION "0.0.1"
 
 // TODO: 256 ANSI color code support
+#define STRING_HIGHLIGHT_COLOR 35
 #define NUMBER_HIGHLIGHT_COLOR 36
 #define SEARCH_MATCH_HIGHLIGHT_COLOR 34
+#define COMMENT_HIGHLIGHT_COLOR 32
 #define DEFAULT_HIGHLIGHT_COLOR 31
 
 #define SIDE_CHARACTER "~"
@@ -13,6 +15,7 @@
 #define QUIT_TIMES 1
 #define EXIT_KEY 113 // q
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 #define CTRL_KEY(k) (k & 0x1f)
 
 struct positions {
